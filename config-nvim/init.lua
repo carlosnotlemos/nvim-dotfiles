7c027c5e25da07505320cf8fd8f255630efcb5e0
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-03-20 21:17:06 -0300

    style: Change Theme Tokyonight-storm

--- config-nvim/init.lua
+++ config-nvim/init.lua
@@ -194,7 +194,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
